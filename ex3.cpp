1ee85674efd01e4d9278e685b9ccc12d3e065c6d
tanya9779 <tanya9779@mail.ru>
2016-09-15 14:24:02 +0300

    ex3 added

---
+++ ex3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+// using namespace std;
+
+
+void good_incrementor(int &x);
+     
+
+int main()
+{
+        int y = 1;
+        std::cout << "x initial: " << y << std::endl;
+        good_incrementor(y);
+        std::cout << "x changed: " << y << std::endl;
+
+        return 0;
+}
+
+void good_incrementor(int &x) // параметр передается по ссылке
+{
+  x++; // обращаться по адресу не нужно   
+}     
