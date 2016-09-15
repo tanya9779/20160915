ee7f9736a49883d86e555e709dade14d11013716
tanya9779 <tanya9779@mail.ru>
2016-09-15 14:04:36 +0300

    first commit

---
+++ ex1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+void bad_incrementor(int x)
+{
+        x++;
+        cout << "x changed: " << x << endl;
+}
+
+int main()
+{
+    int x = 3; // формальный параметр для bad_incrementor()
+    int y = 5; // фактический параметр
+
+    bad_incrementor(y);
+    cout << "y NOT changed: "<< y;
+
+    return 0;
+}
